87ab9891535a4d83b1d292c627fd0244d3fc10d3
simranlotey <simran642003@gmail.com>
2021-12-14 04:11:02 +0530

    React Currency Converter

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import spinner from './Images/spinner.gif'
 function Spinner() {
     return (
         <div className='text-center'>
-            <img src={spinner} alt='' width={"60px"} />
+            <img src={spinner} alt='' width={"60px"} style={{ "marginBottom": "30px" }} />
         </div>
     )
 }
