f7de4f81a21de7decfc47e4d49dd681ed9791ae9
simranlotey <simran642003@gmail.com>
2022-03-09 17:40:06 +0530

    React Currency Converter

--- src/components/Dropdowns.js
+++ src/components/Dropdowns.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { language } from '../config/language'
 
-
+const label = {
+  "width": "325px",
+  "color": "white",
+  "fontSize": "21px"
+}
 function Dropdowns({ labelName, handleChange, value }) {
   return (
 
     <>
-      <label className="dropdown" style={{ "width": "325px", "color": "white", "fontSize": "21px" }}>
+      <label className="dropdown" style={label}>
         {labelName}
         <select
           className='form-select bg-dark custom-select form-select-lg text-white border-dark shadow'
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import spinner from './Images/spinner.gif'
 
+
+const image = {
+    "marginBottom": "30px",
+    "width": "60px"
+}
 function Spinner() {
     return (
         <div className='text-center'>
-            <img src={spinner} alt='' width={"60px"} style={{ "marginBottom": "30px" }} />
+            <img src={spinner} alt="" style={image} />
         </div>
     )
 }
