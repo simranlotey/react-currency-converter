86ba643944d3589d289120ad10079b4b6ad653bb
simranlotey <simran642003@gmail.com>
2023-09-09 23:36:12 +0530

    React Currency Converter

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const heading: string = "currency converter";
 export const API_DOMAIN: string = "https://api.apilayer.com/fixer/convert?to=";
-export const API_KEY: string = "D04rYMknELftvvkPZoC3byCArdtYagFk";
+export const API_KEY: string = "USE-YOUR-API-KEY";
 export const endpointPath = (from: string, to: string, amount: number): string =>
   `${API_DOMAIN}${to}&from=${from}&amount=${amount}`;
